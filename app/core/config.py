44dec24405ce646adc9c94032fd618ad3cf167f9
Daniil Tanygin <xdezerusx@gmail.com>
2025-11-11 22:13:24 +0300

    chore: add DATABASE_TEST_URL setting
    Introduced DATABASE_TEST_URL for test database connection in Settings class.

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:postgres@db:5432/qadb"
+    DATABASE_TEST_URL: str = "postgresql://postgres:postgres@db_test:5432/qadb_test"
 
     class Config:
         env_file = ".env"
