3b90647cf9ec93062d93914ea4e9cf0d8d74521c
Daniil Tanygin <xdezerusx@gmail.com>
2025-11-13 13:23:04 +0300

    refactor: update CRUD layer to use custom exceptions

--- app/crud/answer.py
+++ app/crud/answer.py
@@ -1,20 +1,34 @@
 from sqlalchemy.orm import Session
 from app.models.answer import Answer
 from app.schemas.answer import AnswerCreate
+from app.core.exceptions import AnswerNotFoundException, QuestionNotFoundException
+from app.crud import question as crud_question
+
 
 def get_answer(db: Session, answer_id: int):
-    return db.query(Answer).filter(Answer.id == answer_id).first()
+    answer = db.query(Answer).filter(Answer.id == answer_id).first()
+    if not answer:
+        raise AnswerNotFoundException(answer_id)
+    return answer
+
 
 def create_answer(db: Session, question_id: int, answer: AnswerCreate):
+    # Проверяем существование вопроса
+    db_question = crud_question.get_question(db, question_id)
+    if not db_question:
+        raise QuestionNotFoundException(question_id)
+
     db_answer = Answer(text=answer.text, user_id=answer.user_id, question_id=question_id)
     db.add(db_answer)
     db.commit()
     db.refresh(db_answer)
     return db_answer
 
+
 def delete_answer(db: Session, answer_id: int):
     answer = db.query(Answer).filter(Answer.id == answer_id).first()
-    if answer:
-        db.delete(answer)
-        db.commit()
+    if not answer:
+        raise AnswerNotFoundException(answer_id)
+    db.delete(answer)
+    db.commit()
     return answer
--- app/crud/question.py
+++ app/crud/question.py
@@ -1,12 +1,16 @@
 from sqlalchemy.orm import Session
 from app.models.question import Question
 from app.schemas.question import QuestionCreate
+from app.core.exceptions import QuestionNotFoundException
 
 def get_all_questions(db: Session):
     return db.query(Question).all()
 
 def get_question(db: Session, question_id: int):
-    return db.query(Question).filter(Question.id == question_id).first()
+    question = db.query(Question).filter(Question.id == question_id).first()
+    if not question:
+        raise QuestionNotFoundException(question_id)
+    return question
 
 def create_question(db: Session, question: QuestionCreate):
     db_question = Question(text=question.text)
@@ -17,7 +21,8 @@ def create_question(db: Session, question: QuestionCreate):
 
 def delete_question(db: Session, question_id: int):
     question = db.query(Question).filter(Question.id == question_id).first()
-    if question:
-        db.delete(question)
-        db.commit()
+    if not question:
+        raise QuestionNotFoundException(question_id)
+    db.delete(question)
+    db.commit()
     return question
